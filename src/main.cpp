924f1060a796b36bbdce5d39e6d670d4dcb137c9
Thirtyseventrees <thirtyseventrees@gmail.com>
2025-02-21 20:20:05 +0000

    1

--- src/main.cpp
+++ src/main.cpp
@@ -14,22 +14,9 @@
 #include "../include/rb_tree.h"
 #include "../include/set.h"
 #include "../include/map.h"
+#include "../include/hashtable.h"
 
 int main(){
-
-    mystl::map<int, std::string> a;
-    a[0] = "yu";
-    a[7] = "ft";
-    a[2] = "so";
-    a[1] = "zu";
-
-    for(auto i : a)
-        std::cout << i.second;
-    std::cout << std::endl;
-
-    mystl::map<int, std::string> b;
-    b[5] = "ciallo";
-    std::cout << b[5] << std::endl;
 
     return 0;
 }
